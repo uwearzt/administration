daa8a987a798c9d46b05bb9999e05c2007179e63
Uwe Arzt <mail@uwe-arzt.de>
2019-07-04 09:49:49 +0200

    switch back to zsh

--- ansible/roles/base/files/fish.functions/dir_in_git_repo.fish
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------------------------------------------
-# Copyright 2018 Uwe Arzt, mail@uwe-arzt.de
-# SPDX-License-Identifier: Apache-2.0
-# ------------------------------------------------------------------------------
-
-function dir_in_git_repo --argument directory
-  git -C "$directory" rev-parse --is-inside-working-tree >/dev/null ^/dev/null
-end
--- ansible/roles/base/files/fish.functions/git_branch.fish
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------------------------------------------
-# Copyright 2018 Uwe Arzt, mail@uwe-arzt.de
-# SPDX-License-Identifier: Apache-2.0
-# ------------------------------------------------------------------------------
-
-function git_branch
-  git rev-parse --abbrev-ref HEAD 2>/dev/null
-end
